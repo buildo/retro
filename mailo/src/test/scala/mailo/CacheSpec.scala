3bc4f33f456891fc9ab3feb7f0309f7639f142d1
Claudio Caletti <caletti.claudio@gmail.com>
2016-06-13 14:18:47 +0200

    Add cache tests

---
+++ mailo/src/test/scala/mailo/CacheSpec.scala
@@ -0,0 +1,51 @@
+package mailo
+
+import org.scalatest.{ FlatSpec, Matchers }
+import org.scalatest.concurrent.{ ScalaFutures, Eventually}
+
+import mailo.data.S3MailData
+
+import scalacache._
+import guava._
+import scala.concurrent.duration._
+
+//It's a test, this is fine
+import scala.concurrent.ExecutionContext.Implicits.global
+
+//NOTE: run tests in this suite serially
+class CacheSpec extends FlatSpec with Matchers with ScalaFutures with Eventually{
+
+  implicit val defaultPatience =
+      PatienceConfig(timeout = 20.seconds, interval = 5.seconds)
+
+  implicit val scalaCache = ScalaCache(GuavaCache())
+  private[this] val s3 = new S3MailData()
+
+  val templateName1 = "mail.html"
+  val templateName2 = "mail-image.html"
+
+  "cache" should "initially be empty" in {
+    get[String, NoSerialization](templateName1).futureValue should be (None)
+    get[String, NoSerialization](templateName2).futureValue should be (None)
+  }
+
+  "cache" should "be populated correctly " in {
+    cachingWithTTL(templateName1)(10.seconds) {
+      s3.get(templateName1)
+    }.futureValue
+
+    cachingWithTTL(templateName2)(30.seconds) {
+      s3.get(templateName2)
+    }.futureValue
+
+    get[String, NoSerialization](templateName2).futureValue should not be (None)
+    get[String, NoSerialization](templateName1).futureValue should not be (None)
+  }
+
+  "eventually the cache of the first template" should "expire" in {
+    eventually (timeout(20.seconds)) {
+      get[String, NoSerialization](templateName1).futureValue should be (None)
+      get[String, NoSerialization](templateName2).futureValue should not be (None)
+    }
+  }
+}
