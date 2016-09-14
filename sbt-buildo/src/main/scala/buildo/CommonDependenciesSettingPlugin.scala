fd1e2e9d4b422ecbcce3414c4d9e52cd86c5431e
Gabriele Petronella <gabriele@buildo.io>
2016-09-14 19:11:51 +0300

    Add more dependencies to defaultDependencies

--- sbt-buildo/src/main/scala/buildo/CommonDependenciesSettingPlugin.scala
+++ sbt-buildo/src/main/scala/buildo/CommonDependenciesSettingPlugin.scala
@@ -13,13 +13,21 @@ object CommonDependencySettingPlugin extends AutoPlugin {
     val slick = "com.typesafe.slick" %% "slick" % "3.1.1"
     val hikari = "com.typesafe.slick" %% "slick-hikaricp" % "3.1.0"
     val logging = "io.buildo" %% "ingredients-logging" % "0.6.0"
+    val sprayJson = "io.spray" %% "spray-json" % "1.3.2"
+
+    val scalaTest = "org.scalatest" %% "scalatest" % "3.0.0-RC1" % "test"
+    val sprayTest = "io.spray" %% "spray-testkit" % "1.3.3" % "test"
+    val metarpheusAnnotations = "io.buildo" %% "metarpheus-annotations" % "0.0.1"
 
     val defaultDependencies = Seq(
       nozzle,
       scalaz,
       slick,
       hikari,
-      logging
+      logging,
+      sprayJson,
+      scalaTest,
+      sprayTest
     )
 
   }
