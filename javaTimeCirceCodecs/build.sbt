1baed5f66fbc25faa1427af9f943ba862408f3df
Claudio Caletti <claudio@buildo.io>
2017-04-14 18:01:41 +0200

    Add TimeCirceSupport

---
+++ javaTimeCirceCodecs/build.sbt
@@ -0,0 +1,24 @@
+enablePlugins(GitVersioning)
+
+lazy val commonSettings = Seq(
+  organization  := "io.buildo",
+  crossScalaVersions := Seq("2.11.8", "2.12.1"),
+  scalaVersion := "2.12.1",
+  licenses += ("MIT", url("http://opensource.org/licenses/MIT")),
+  bintrayOrganization := Some("buildo"),
+  bintrayVcsUrl := Some("git@github.com:buildo/java-time-circe-codecs")
+)
+
+val circeVersion = "0.7.0"
+
+lazy val javaTimeCirceCodecs = project
+  .settings(commonSettings)
+  .settings(
+    name := "java-time-circe-codecs",
+    libraryDependencies ++= Seq(
+      "io.circe" %% "circe-core" % circeVersion,
+      "io.circe" %% "circe-java8" % circeVersion
+    )
+  )
+
+addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full)
---
+++ javaTimeCirceCodecs/javaTimeCirceCodecs/src/main/scala/TimeCirceSupport.scala
@@ -0,0 +1,19 @@
+package io.buildo
+
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatterBuilder
+
+import io.circe.{ Decoder, Encoder }
+
+import io.circe.java8.time.TimeInstances
+
+trait TimeCirceSupport extends TimeInstances {
+  private[this] val dateTimeFormatterBuilder = new DateTimeFormatterBuilder
+  //Always have 3 fractional digits
+  //.appendInstant converts to a data-time with a zone-offset of UTC formatted as ISO-8601
+  private[this] val dateTimeFormatter = dateTimeFormatterBuilder.appendInstant(3).toFormatter
+
+  //overrides default circe encoder/decoder
+  implicit val decodeOffsetDateTime: Decoder[OffsetDateTime] = decodeOffsetDateTime(dateTimeFormatter)
+  implicit val encodeOffsetDateTime: Encoder[OffsetDateTime] = encodeOffsetDateTime(dateTimeFormatter)
+}
