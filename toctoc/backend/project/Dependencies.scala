7d382bf972d66dc88b422ab86e47fcda4c33d131
Daniele Gallingani <daniele@buildo.io>
2017-10-03 16:31:41 +0200

    add slickVersion variable

--- toctoc/backend/project/Dependencies.scala
+++ toctoc/backend/project/Dependencies.scala
@@ -1,10 +1,11 @@
 import sbt._
 
 object Dependencies {
+  lazy val slickVersion = "3.2.1"
   lazy val slick = Seq(
-    "com.typesafe.slick" %% "slick" % "3.2.1",
+    "com.typesafe.slick" %% "slick" % slickVersion,
     "org.slf4j" % "slf4j-nop" % "1.6.4",
-    "com.typesafe.slick" %% "slick-hikaricp" % "3.2.1"
+    "com.typesafe.slick" %% "slick-hikaricp" % slickVersion
   )
   lazy val postgresql = Seq("org.postgresql" % "postgresql" % "9.4.1212")
   lazy val enumero = Seq("io.buildo" %% "enumero" % "1.1.0")
