f1e4c4da4ab1555a804fc5db0dec582472598177
Giuseppe Moscarella <giuseppe.moscarella@buildo.io>
2017-10-13 17:29:33 +0200

    metarpheus models and apis

--- toctoc/web/src/metarpheus/api.js
+++ toctoc/web/src/metarpheus/api.js
@@ -7,5 +7,48 @@ import * as m from './model';
 
 
 export default [
+  // POST /toctoc/login : 
+  {
+    method: 'post',
+    name: ['tokenAuthenticationController', 'login'],
+    authenticated: false,
+    returnType: m.TocTocToken,
+    route: (...routeParams) => ['toctoc', 'login'].join('/'),
+    routeParamTypes: [],
+    params: {
+      
+    },
+    body: t.interface({
+      login: m.Login
+    })
+  },
 
+  // POST /toctoc/logout : 
+  {
+    method: 'post',
+    name: ['tokenAuthenticationController', 'logout'],
+    authenticated: true,
+    returnType: t.Nil,
+    route: (...routeParams) => ['toctoc', 'logout'].join('/'),
+    routeParamTypes: [],
+    params: {
+      
+    }
+  },
+
+  // POST /toctoc/refresh : 
+  {
+    method: 'post',
+    name: ['tokenAuthenticationController', 'refresh'],
+    authenticated: false,
+    returnType: m.TocTocToken,
+    route: (...routeParams) => ['toctoc', 'refresh'].join('/'),
+    routeParamTypes: [],
+    params: {
+      
+    },
+    body: t.interface({
+      refreshToken: m.RefreshToken
+    })
+  }
 ];
--- toctoc/web/src/metarpheus/model.js
+++ toctoc/web/src/metarpheus/model.js
@@ -4,6 +4,31 @@
 import t from 'tcomb';
 
 
+export const AccessToken = t.declare('AccessToken');
 
+export const Login = t.declare('Login');
 
+export const RefreshToken = t.declare('RefreshToken');
+
+export const TocTocToken = t.declare('TocTocToken');
+
+AccessToken.define(t.struct({
+  value: t.String,
+  expiresAt: Instant
+}));
+
+Login.define(t.struct({
+  username: t.String,
+  password: t.String
+}));
+
+RefreshToken.define(t.struct({
+  value: t.String,
+  expiresAt: Instant
+}));
+
+TocTocToken.define(t.struct({
+  accessToken: AccessToken,
+  refreshToken: RefreshToken
+}));
   
