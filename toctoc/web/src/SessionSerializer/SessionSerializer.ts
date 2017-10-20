b989e1f759f09b249d2cb985eef5d47a3b0efeb4
Vincenzo Guerrisi <vincenzo@buildo.io>
2017-10-20 16:55:27 +0200

    serializationKey is fixed to 'AUTH_TOKEN'

--- toctoc/web/src/SessionSerializer/SessionSerializer.ts
+++ toctoc/web/src/SessionSerializer/SessionSerializer.ts
@@ -2,13 +2,16 @@ type SessionSetter = (key: string, jsonValue: string) => void
 type SessionGetter = (key: string) => string | null
 
 export default function SessionSerializer({ getter, setter }: { getter: SessionGetter, setter: SessionSetter }) {
+const serializationKey = 'AUTH_TOKEN'
   return {
     serialize<T>(value: T, key: string = 'AUTH_TOKEN'): void {
       setter(key, JSON.stringify(value))
+      setter(serializationKey, JSON.stringify(value))
     },
 
     deserialize<T>(key: string = 'AUTH_TOKEN'): T {
       return JSON.parse(getter(key) || 'null')
+      return JSON.parse(getter(serializationKey) || 'null')
     }
   }
 }
