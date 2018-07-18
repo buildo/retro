d01fd49e09b64be6b7aa2f2a275cd1ceed776c0b
Claudio Caletti <claudio@buildo.io>
2018-07-18 17:36:58 +0200

    Dispatch

--- mailo/src/main/scala/mailo/AtMostOnceMailo.scala
+++ mailo/src/main/scala/mailo/AtMostOnceMailo.scala
@@ -1,5 +1,58 @@
 package mailo
 
-class AtMostOnceMailo {
+import cats.data.EitherT
+import cats.instances.future._
+import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.scalalogging.LazyLogging
+import mailo.MailRefinedContent._
+import mailo.data.MailData
+import mailo.http.MailClient
+import mailo.parser.HTMLParser
+import scalacache._
+import scalacache.guava._
 
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext
+
+class AtMostOnceMailo(
+  val mailData: MailData,
+  val mailClient: MailClient,
+)(
+  implicit
+  ec: ExecutionContext,
+  conf: Config = ConfigFactory.load()
+) extends Mailo with LazyLogging {
+  implicit private[this] val scalaCache = ScalaCache(GuavaCache())
+
+  private[this] case class MailoConfig(cachingTTLSeconds: Int)
+  private[this] val mailoConfig = MailoConfig(
+    cachingTTLSeconds = conf.getInt("mailo.cachingTTLSeconds")
+  )
+
+  def send(mail: Mail) = {
+    val result = for {
+      content <- EitherT(cachingWithTTL(mail.templateName)(mailoConfig.cachingTTLSeconds.seconds) {
+        mailData.get(mail.templateName)
+      })
+      _ = logger.debug(s"retrieved ${mail.templateName} content")
+      parsedContent <- EitherT.fromEither(HTMLParser.parse(content, mail.params))
+      _ = logger.debug(s"template populated with params: ${mail.params}")
+      result <- EitherT(
+        mailClient.send(
+          to = mail.to,
+          from = mail.from,
+          cc = mail.cc,
+          bcc = mail.bcc,
+          subject = mail.subject,
+          content = HTMLContent(parsedContent),
+          attachments = mail.attachments,
+          tags = mail.tags,
+          headers = mail.headers
+        )
+      )
+      _ = logger.info(s"email sent with id: ${result.id}")
+    } yield result
+
+    result.value
+  }
 }
--- mailo/src/main/scala/mailo/Helpers.scala
+++ mailo/src/main/scala/mailo/Helpers.scala
@@ -1,5 +1,75 @@
 package mailo
 
-class Helpers {
+import java.io.{PrintWriter, StringWriter}
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import mailo.data.S3MailData
+import mailo.http.MailgunClient
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class S3MailgunMailo(
+  implicit
+  system: ActorSystem,
+  materializer: ActorMaterializer,
+  ec: ExecutionContext
+) {
+  private[this] val s3 = new S3MailData()
+  private[this] val mailgun = new MailgunClient()
+
+  private[this] val mailgunS3Mailo = new AtMostOnceMailo(s3, mailgun)
+
+  def send(
+    to: String,
+    from: String,
+    cc: Option[String] = None,
+    bcc: Option[String] = None,
+    subject: String,
+    templateName: String,
+    params: Map[String, String],
+    attachments: List[Attachment] = Nil,
+    tags: List[String] = Nil
+  ): Future[Either[MailError, MailResponse]] =
+    mailgunS3Mailo.send(Mail(to, from, cc, bcc, subject, templateName, params, attachments, tags))
+}
+
+class S3SendinblueMailo(
+  implicit
+  system: ActorSystem,
+  materializer: ActorMaterializer,
+  ec: ExecutionContext
+) {
+  import data.S3MailData
+  import http.SendinblueClient
+
+  private[this] val s3 = new S3MailData()
+  private[this] val sendinblue = new SendinblueClient()
+
+  private[this] val sendinblueS3Mailo = new AtMostOnceMailo(s3, sendinblue)
+
+  def send(
+    to: String,
+    from: String,
+    cc: Option[String] = None,
+    bcc: Option[String] = None,
+    subject: String,
+    templateName: String,
+    params: Map[String, String],
+    attachments: List[Attachment] = Nil,
+    tags: List[String] = Nil
+  ): Future[Either[MailError, MailResponse]] =
+    sendinblueS3Mailo.send(
+      Mail(to, from, cc, bcc, subject, templateName, params, attachments, tags)
+    )
+}
+
+package object util {
+  implicit class PimpThrowable(t: Throwable) {
+    def getStackTraceAsString: String = {
+      val sw = new StringWriter
+      t.printStackTrace(new PrintWriter(sw))
+      sw.toString
+    }
+  }
 }
