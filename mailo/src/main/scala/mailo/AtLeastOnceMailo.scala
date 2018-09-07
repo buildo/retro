6f29ae3770ecd02e06ad05cb0dd53a22370b01af
Claudio Caletti <caletti.claudio@gmail.com>
2018-09-07 20:07:40 +0200

    Ask for persistance

--- mailo/src/main/scala/mailo/AtLeastOnceMailo.scala
+++ mailo/src/main/scala/mailo/AtLeastOnceMailo.scala
@@ -2,16 +2,16 @@ package mailo
 
 import akka.actor.{ActorSystem, Props}
 import akka.util.Timeout
+import akka.pattern.ask
 import com.typesafe.config.{Config, ConfigFactory}
 import com.typesafe.scalalogging.LazyLogging
 import mailo.data.MailData
 import mailo.http.MailClient
+import mailo.persistence.{EmailPersistorActor, SendEmail}
+
 import scala.concurrent.duration._
-
 import scala.util.{Failure, Success}
-
 import scala.concurrent.{ExecutionContext, Future}
-
 import scala.language.postfixOps
 
 case class MailPersistenceError(message: String) extends MailError(message)
@@ -28,11 +28,10 @@ class AtLeastOnceMailo(
     with LazyLogging {
   implicit val timeout = Timeout(5 seconds)
 
-  //return type of actor ask pattern
-  def ask: Future[Any] = ???
+  private[this] val persisto = system.actorOf(Props[EmailPersistorActor])
 
   def send(mail: Mail) = {
-    ask.map {
+    ask(persisto, SendEmail(mail)).map {
       case Success(result) => Right(LocallyQueued)
       case Failure(error)  => Left(MailPersistenceError(error.getLocalizedMessage))
     }
