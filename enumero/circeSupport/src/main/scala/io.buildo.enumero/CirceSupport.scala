ccfea52a67c34197039121963114708d07407057
Federico Sordillo <federicosordillo@gmail.com>
2019-01-16 18:10:01 +0100

    added encoding for enum maps

--- enumero/circeSupport/src/main/scala/io.buildo.enumero/CirceSupport.scala
+++ enumero/circeSupport/src/main/scala/io.buildo.enumero/CirceSupport.scala
@@ -1,35 +1,56 @@
 package io.buildo.enumero
 
+import io.circe.{KeyDecoder, KeyEncoder}
 import io.circe._
 
 trait CirceSupport {
 
-  private[this] def deserializationError(actual: String, expected: String): String =
+  private[this] def deserializationError(actual: String,
+                                         expected: String): String =
     s"Expected a value of type ${expected}, but got ${actual}"
 
-  private[this] def deserializationErrorForIndex(actual: String, expected: String): String =
+  private[this] def deserializationErrorForIndex(actual: String,
+                                                 expected: String): String =
     s"Expected an indexed value for type ${expected}, but got ${actual}"
 
-  implicit def encodeCaseEnum[E <: CaseEnum](implicit s: CaseEnumSerialization[E]): Encoder[E] =
+  implicit def encodeCaseEnum[E <: CaseEnum](
+      implicit s: CaseEnumSerialization[E]): Encoder[E] =
     Encoder.encodeString.contramap[E](s.caseToString)
 
-  implicit def decodeCaseEnum[E <: CaseEnum](implicit s: CaseEnumSerialization[E]): Decoder[E] =
+  implicit def decodeCaseEnum[E <: CaseEnum](
+      implicit s: CaseEnumSerialization[E]): Decoder[E] =
     Decoder.decodeString.emap { str =>
-      s.caseFromString(str).map(Right(_)).getOrElse(Left(deserializationError(str, s.name)))
+      s.caseFromString(str)
+        .map(Right(_))
+        .getOrElse(Left(deserializationError(str, s.name)))
     }
 
+  implicit final def decodeKeyCaseEnum[E <: CaseEnum](
+      implicit s: CaseEnumSerialization[E]
+  ): KeyDecoder[E] =
+    KeyDecoder.instance[E](s.caseFromString)
+
+  implicit final def encodeKeyCaseEnum[E <: CaseEnum](
+      implicit s: CaseEnumSerialization[E]
+  ): KeyEncoder[E] =
+    KeyEncoder.instance[E](s.caseToString)
+
   implicit def encodeIndexedCaseEnum[E <: IndexedCaseEnum](
-    implicit c: CaseEnumIndex[E], e: Encoder[E#Index]
+      implicit c: CaseEnumIndex[E],
+      e: Encoder[E#Index]
   ): Encoder[E] = e.contramap(c.caseToIndex)
 
   implicit def decodeIndexedCaseEnum[E <: IndexedCaseEnum](
-    implicit c: CaseEnumIndex[E], s: CaseEnumSerialization[E], d: Decoder[E#Index]
+      implicit c: CaseEnumIndex[E],
+      s: CaseEnumSerialization[E],
+      d: Decoder[E#Index]
   ): Decoder[E] = d.emap { index =>
-    c.caseFromIndex(index).map(Right(_)).getOrElse(
-      Left(deserializationErrorForIndex(index.toString, s.name))
-    )
+    c.caseFromIndex(index)
+      .map(Right(_))
+      .getOrElse(
+        Left(deserializationErrorForIndex(index.toString, s.name))
+      )
   }
-
 }
 
 package object circe extends CirceSupport
---
+++ enumero/circeSupport/src/test/scala/CirceSupportSpec.scala
@@ -0,0 +1,48 @@
+import io.buildo.enumero._
+import io.buildo.enumero.circe._
+import io.circe._, io.circe.syntax._, io.circe.parser._
+
+import org.scalatest.{Matchers, WordSpec}
+
+class CirceSupportSpec extends WordSpec with Matchers {
+  sealed trait Planet extends CaseEnum
+  object Planet {
+    case object Mercury extends Planet
+    case object Venus extends Planet
+    case object Earth extends Planet
+  }
+
+  val planetMap = Map[Planet, Int](
+    Planet.Mercury -> 12,
+    Planet.Venus -> 812763,
+    Planet.Earth -> 0
+  )
+
+  val json = planetMap.asJson
+
+  "CirceSupport handles encoding of a map with CaseEnum keys" in {
+    json shouldBe parse("""
+      {
+        "Mercury": 12,
+        "Venus": 812763,
+        "Earth": 0
+      }
+    """).getOrElse(Json.Null)
+  }
+
+  "CirceSupport handles dencoding of a json with CaseEnum keys" in {
+    json.as[Map[Planet, Int]].getOrElse(Json.Null) shouldBe planetMap
+  }
+
+  "CirceSupport handles dencoding of a json with wrong CaseEnum keys" in {
+    parse("""
+      {
+        "Mercury": 12,
+        "Venus": 812763,
+        "wrongKey": 0
+      }
+    """)
+      .getOrElse(Json.Null)
+      .as[Map[Planet, Int]] shouldBe a [Left[_, DecodingFailure]]
+  }
+}
