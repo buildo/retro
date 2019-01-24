0502ec30d2b2fb367eda2e9837a670b99223e73c
Cardelli Emanuele <cardu90@yahoo.it>
2019-01-24 10:32:23 +0100

    istant typo fix

--- javaTimeCirceCodecs/javaTimeCirceCodecs/src/test/scala/FormatterSpecs.scala
+++ javaTimeCirceCodecs/javaTimeCirceCodecs/src/test/scala/FormatterSpecs.scala
@@ -8,20 +8,20 @@ import io.circe.syntax._
 import org.scalatest._
 
 class FormatterSpecs extends FlatSpec with Matchers with TimeCirceSupport {
-  val testIstant = Instant.ofEpochMilli(1548265709578l)
+  val testInstant = Instant.ofEpochMilli(1548265709578l)
   def filterReversedMillis(j : Json) = j.noSpaces.reverse.takeWhile(p => p != '.').filter(_.isDigit)
 
-  "Istant" should "be printed in json with just 3 millis" in {
-    filterReversedMillis(testIstant.asJson) should have length 3
+  "Instant" should "be printed in json with just 3 millis" in {
+    filterReversedMillis(testInstant.asJson) should have length 3
   }
 
   "LocalDateTime" should "be printed in json with just 3 millis" in {
-    val date = LocalDateTime.ofInstant(testIstant, ZoneId.systemDefault())
+    val date = LocalDateTime.ofInstant(testInstant, ZoneId.systemDefault())
     filterReversedMillis(date.asJson) should have length 3
   }
 
   "OffsetDateTime" should "be printed in json with just 3 millis" in {
-    val offset = OffsetDateTime.ofInstant(testIstant, ZoneId.systemDefault())
+    val offset = OffsetDateTime.ofInstant(testInstant, ZoneId.systemDefault())
     filterReversedMillis(offset.asJson) should have length 3
   }
 }
