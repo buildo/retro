785dc2b1eafd6b7e2a9653b7df345336de21460a
Claudio Caletti <claudio@buildo.io>
2019-01-28 18:22:13 +0100

    Add concurrent test

--- mailo/src/test/scala/mailo/PersistenceSpec.scala
+++ mailo/src/test/scala/mailo/PersistenceSpec.scala
@@ -1,8 +1,12 @@
 package mailo
 
-import akka.actor.{ActorSystem, PoisonPill}
+import java.util.concurrent.ConcurrentLinkedQueue
+
+import akka.actor.{ActorSystem, PoisonPill, Kill}
 import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
 import akka.testkit.{TestKit, TestProbe, ImplicitSender}
+
+import akka.persistence.inmemory.extension.{ InMemoryJournalStorage, InMemorySnapshotStorage, StorageExtension }
 
 import cats.syntax.either._
 
@@ -10,7 +14,7 @@ import mailo.http.MailClient
 import mailo.data.MailData
 import mailo.persistence.{EmailPersistanceActor, SendEmail, LoggingActor}
 
-import org.scalatest.{Matchers, BeforeAndAfterAll, Suite, WordSpecLike, FeatureSpec}
+import org.scalatest.{Matchers, BeforeAndAfterEach, BeforeAndAfterAll, Suite, WordSpecLike, FeatureSpec}
 import org.scalatest.concurrent.{ScalaFutures, Eventually}
 import org.scalatest.time.{Span, Seconds}
 
@@ -18,8 +22,13 @@ import scala.collection.mutable.Queue
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
 
+import akka.pattern.ask
+
+import akka.util.Timeout
+
 case class SimpleMail(subject: String)
-class MockedClient(val state: Queue[SimpleMail]) extends MailClient {
+
+class MockedClient(val state: ConcurrentLinkedQueue[SimpleMail]) extends MailClient {
   override def send(
     to: String,
     from: String,
@@ -32,10 +41,27 @@ class MockedClient(val state: Queue[SimpleMail]) extends MailClient {
     headers: Map[String, String]
   )(implicit executionContext: ExecutionContext): Future[Either[MailError, MailResponse]] = Future.successful {
     //State needs to be updated by one at the time
-    synchronized {
-      state.enqueue(SimpleMail(subject))
-      Right(MailResponse(subject, "ok"))
-    }
+    state.add(SimpleMail(subject))
+    Right(MailResponse(subject, "ok"))
+  }
+}
+
+class MockedClientWithDelay(val state: ConcurrentLinkedQueue[SimpleMail]) extends MailClient {
+  override def send(
+    to: String,
+    from: String,
+    cc: Option[String],
+    bcc: Option[String],
+    subject: String,
+    content: MailRefinedContent.MailRefinedContent,
+    attachments: List[Attachment],
+    tags: List[String],
+    headers: Map[String, String]
+  )(implicit executionContext: ExecutionContext): Future[Either[MailError, MailResponse]] = Future.successful {
+    //State needs to be updated by one at the time
+    Thread sleep 200
+    state.add(SimpleMail(subject))
+    Right(MailResponse(subject, "ok"))
   }
 }
 
@@ -52,6 +78,7 @@ class PersistenceSpec
   with WordSpecLike
   with Matchers
   with BeforeAndAfterAll
+  with BeforeAndAfterEach
   with ScalaFutures {
   import Eventually._
 
@@ -61,12 +88,18 @@ class PersistenceSpec
     TestKit.shutdownActorSystem(system)
   }
 
+  override def beforeEach(): Unit = {
+    val tp = TestProbe()
+    tp.send(StorageExtension(system).journalStorage, InMemoryJournalStorage.ClearJournal)
+    tp.send(StorageExtension(system).snapshotStorage, InMemorySnapshotStorage.ClearSnapshots)
+  }
+
   implicit val defaultPatience =
       PatienceConfig(timeout = Span(20, Seconds), interval = Span(5, Seconds))
 
   "persistence actor" should {
     "properly deliver email messages" in {
-      val state = new Queue[SimpleMail]()
+      val state = new ConcurrentLinkedQueue[SimpleMail]()
       val emailSender = new EmailSender(new MockedData, new MockedClient(state))
       val loggingActor = system.actorOf(LoggingActor.props())
       val emailPersistanceActor = system.actorOf(EmailPersistanceActor.props(emailSender, loggingActor))
@@ -92,6 +125,48 @@ class PersistenceSpec
         state.size should be(2)
         info(s"${state.size} messages sent")
       }
+    }
+
+    "not explode" in {
+      val state = new ConcurrentLinkedQueue[SimpleMail]()
+      val emailSender = new EmailSender(new MockedData, new MockedClientWithDelay(state))
+      val loggingActor = system.actorOf(LoggingActor.props())
+      val emailPersistanceActor = system.actorOf(EmailPersistanceActor.props(emailSender, loggingActor))
+
+      implicit val enqueueTimeout: Timeout = Timeout(100.milliseconds)
+
+      val mail = Mail(
+        to = "mailo@buildo.io",
+        from = "Mailo mailo@buildo.io",
+        subject = "1",
+        templateName = "mail.html",
+        params = Map.empty,
+        tags = List("test")
+      )
+
+      var queuedEmails = 0
+      info("start")
+
+      val task = system.scheduler.schedule(0.seconds, 1.milliseconds,
+        new Runnable {
+          def run(): Unit = {
+            ask(emailPersistanceActor, SendEmail(mail))
+              .onComplete {
+                case scala.util.Success(result) => queuedEmails +=1
+                case scala.util.Failure(error) => ()
+              }
+        }
+      })
+
+      Thread sleep 5000
+
+      emailPersistanceActor ! Kill
+
+      Thread sleep 200
+      task.cancel()
+
+      queuedEmails should be(state.size)
+      info(s"queued $queuedEmails emails, received ${state.size}")
     }
   }
 }
