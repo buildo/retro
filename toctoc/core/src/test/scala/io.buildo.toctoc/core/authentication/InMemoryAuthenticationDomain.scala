474c19ba5d82198884d97d91fc73d03ac267ce8a
Giuseppe Moscarella <giuseppe.moscarella@buildo.io>
2019-05-22 19:39:49 +0300

    In memory test double for AuthenticationDomain

---
+++ toctoc/core/src/test/scala/io.buildo.toctoc/core/authentication/InMemoryAuthenticationDomain.scala
@@ -0,0 +1,43 @@
+package io.buildo.toctoc
+package core
+package authentication
+
+import cats.Applicative
+import cats.syntax.all._
+
+final class InMemoryAuthenticationDomain[F[_]: Applicative, C <: Credential](
+  private val credentials: Map[C, Subject] = Map.empty,
+) extends AuthenticationDomain[F, C] {
+
+  override def authenticate(
+    c: C,
+  ): F[Either[AuthenticationError, (AuthenticationDomain[F, C], Subject)]] =
+    credentials
+      .get(c)
+      .map((this, _))
+      .toRight(AuthenticationError.InvalidCredential)
+      .pure[F]
+      .widen
+
+  override def register(
+    s: Subject,
+    c: C,
+  ): F[Either[AuthenticationError, AuthenticationDomain[F, C]]] =
+    new InMemoryAuthenticationDomain(credentials + (c -> s))
+      .asRight[AuthenticationError]
+      .pure[F]
+      .widen
+
+  override def unregister(s: Subject): F[Either[AuthenticationError, AuthenticationDomain[F, C]]] =
+    new InMemoryAuthenticationDomain(credentials.filterNot {
+      case (_, v) => v == s
+    }).asRight[AuthenticationError]
+      .pure[F]
+      .widen
+
+  override def unregister(c: C): F[Either[AuthenticationError, AuthenticationDomain[F, C]]] =
+    new InMemoryAuthenticationDomain(credentials - c)
+      .asRight[AuthenticationError]
+      .pure[F]
+      .widen
+}
