d95834c3b3c5104ccbca09085d76f4f72bd111ba
Claudio Caletti <claudio@buildo.io>
2020-01-03 11:40:40 +0100

    Update sbt plugin

--- tapiro/sbt-tapiro/src/main/scala/io/buildo/tapiro/SbtTapiro.scala
+++ tapiro/sbt-tapiro/src/main/scala/io/buildo/tapiro/SbtTapiro.scala
@@ -12,17 +12,19 @@ object SbtTapiro extends AutoPlugin {
 
   object autoImport {
     val tapiro = taskKey[Unit]("Generate tapir endpoints from controller sources")
-    val tapiroControllersPath = settingKey[String]("Path to controller sources")
-    val tapiroEndpointsPath = settingKey[String]("Path to output generated endpoints")
-    val tapiroEndpointsPackage = settingKey[NonEmptyList[String]]("Package of generate endpoints")
+    val tapiroRoutesPaths = settingKey[NonEmptyList[String]]("Paths to the controllers describing the routes")
+    val tapiroModelsPaths = settingKey[List[String]]("Paths to the models used by the controllers")
+    val tapiroOutputPath = settingKey[String]("Path to output generated endpoints")
+    val tapiroEndpointsPackages = settingKey[NonEmptyList[String]]("Packages of generate endpoints")
     val tapiroIncludeHttp4sModels = settingKey[Boolean]("Whether to include http4s generated code")
   }
 
   import autoImport._
 
   override val globalSettings = Seq(
-    tapiro / tapiroControllersPath := "",
-    tapiro / tapiroEndpointsPath := "",
+    tapiro / tapiroRoutesPaths := NonEmptyList("", Nil),
+    tapiro / tapiroModelsPaths := Nil,
+    tapiro / tapiroOutputPath := "",
     tapiro / tapiroIncludeHttp4sModels := true,
   )
 
@@ -30,9 +32,10 @@ object SbtTapiro extends AutoPlugin {
     Seq(
       tapiro := {
         Util.createFiles(
-          (scalaSource.value / tapiroControllersPath.in(tapiro).value).toString,
-          (scalaSource.value / tapiroEndpointsPath.in(tapiro).value).toString,
-          tapiroEndpointsPackage.in(tapiro).value,
+          tapiroRoutesPaths.in(tapiro).value.map(s => (scalaSource.value / s).toString),
+          tapiroModelsPaths.in(tapiro).value.map(s => (scalaSource.value / s).toString),
+          (scalaSource.value / tapiroOutputPath.in(tapiro).value).toString,
+          tapiroEndpointsPackages.in(tapiro).value,
           tapiroIncludeHttp4sModels.in(tapiro).value,
         )
       },
