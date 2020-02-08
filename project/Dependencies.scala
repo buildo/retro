d7919742016752605d388788cae9539c1c5161d4
Scala Steward <me@scala-steward.org>
2020-02-08 20:19:26 +0100

    Update cats-effect to 1.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     val akkaHttpCirce = "1.25.2"
     val awscala = "0.5.+"
     val cats = "1.6.0"
-    val catsEffect = "1.3.0"
+    val catsEffect = "1.3.1"
     val monixCatnap = "3.0.0-RC2"
     val config = "1.3.0"
     val scalacacheGuava = "0.9.3"
