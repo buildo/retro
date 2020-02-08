16adf17e32ae098e37e104afa2de6c14c16434c2
Scala Steward <me@scala-steward.org>
2020-02-08 20:14:59 +0100

    Update typesafe:config to 1.3.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     val cats = "1.6.0"
     val catsEffect = "1.3.0"
     val monixCatnap = "3.0.0-RC2"
-    val config = "1.3.0"
+    val config = "1.3.4"
     val scalacacheGuava = "0.9.3"
     val scalaLogging = "3.8.0"
     val logback = "1.2.3"
