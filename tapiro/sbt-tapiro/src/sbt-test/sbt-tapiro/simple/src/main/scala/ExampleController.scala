0ea5c7b63b860747222976b6c6963dde2ffbbffd
Gabriele Petronella <gabriele@buildo.io>
2020-03-15 15:03:47 +0100

    Add smoke tests for sbt-tapiro

---
+++ tapiro/sbt-tapiro/src/sbt-test/sbt-tapiro/simple/src/main/scala/ExampleController.scala
@@ -0,0 +1,35 @@
+package testo
+
+import cats._
+
+import scala.annotation.StaticAnnotation
+class query extends StaticAnnotation
+class command extends StaticAnnotation
+
+case class CustomError(message: String)
+
+trait ExampleController[F[_], T] {
+  @query
+  def queryExample(
+    intParam: Int,
+    stringParam: String,
+  ): F[Either[CustomError, CustomModel]]
+
+  @command
+  def commandExample(
+    body: CustomModel,
+  ): F[Either[String, String]]
+}
+
+object ExampleController {
+  def create[F[_]: Applicative] = new ExampleController[F, String] {
+    override def commandExample(body: CustomModel): F[Either[String, String]] =
+      Applicative[F].pure(Right(body.name))
+
+    override def queryExample(
+      intParam: Int,
+      stringParam: String,
+    ): F[Either[CustomError, CustomModel]] =
+      Applicative[F].pure(Right(CustomModel(stringParam, intParam.toDouble)))
+  }
+}
