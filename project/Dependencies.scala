9b2206751d2f806f97fd9af5e491d9fbc4ad52ce
Scala Steward <me@scala-steward.org>
2020-07-12 22:11:28 +0200

    Update mysql-connector-java to 8.0.21

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
     val jakartaMail = "1.6.5"
     val slick = "3.3.2"
     val postgresql = "42.2.12"
-    val mysql = "8.0.19"
+    val mysql = "8.0.21"
     val ldap = "4.0.10"
     val flyway = "5.2.4"
     val bcrypt = "0.4"
