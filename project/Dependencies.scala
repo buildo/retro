3cbd9ac04b9d934cac5d29bf4f82245760b2d2b9
Scala Steward <me@scala-steward.org>
2020-11-01 14:12:46 +0100

    Update jakarta.mail to 2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
     val logback = "1.2.3"
     val leveldb = "1.8"
     val mailin = "3.0.1"
-    val jakartaMail = "1.6.5"
+    val jakartaMail = "2.0.0"
     val slick = "3.3.3"
     val postgresql = "42.2.12"
     val mysql = "8.0.22"
