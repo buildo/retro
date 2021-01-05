6dd230d776b9f5cc422af49bbd545ab265883265
Scala Steward <me@scala-steward.org>
2021-01-05 12:11:42 +0100

    Update akka-http-circe to 1.35.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val mockito = "1.9.5"
     val akka = "2.6.4"
     val akkaHttp = "10.1.11"
-    val akkaHttpCirce = "1.35.2"
+    val akkaHttpCirce = "1.35.3"
     val awscala = "0.5.+"
     val cats = "1.6.0"
     val catsEffect = "1.3.0"
