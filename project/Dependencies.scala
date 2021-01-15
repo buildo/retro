277eaf3ddaf5c282ed0e42fc8a056155c925c892
Scala Steward <me@scala-steward.org>
2021-01-15 14:15:01 +0100

    Update akka-persistence, akka-remote, ... to 2.6.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val scalacheck = "1.14.0"
     val scalacheckMagnolia = "0.4.0"
     val mockito = "1.9.5"
-    val akka = "2.6.4"
+    val akka = "2.6.11"
     val akkaHttp = "10.1.11"
     val akkaHttpCirce = "1.35.2"
     val awscala = "0.5.+"
