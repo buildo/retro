976923966d95d3d2adf3783c08b474da31202e36
Scala Steward <me@scala-steward.org>
2021-01-17 16:09:07 +0100

    Update akka-http to 10.1.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val scalacheckMagnolia = "0.6.0"
     val mockito = "1.9.5"
     val akka = "2.6.11"
-    val akkaHttp = "10.1.11"
+    val akkaHttp = "10.1.13"
     val akkaHttpCirce = "1.35.3"
     val awscala = "0.5.+"
     val cats = "1.6.0"
