d0b48e10e1408cf0ec9aa3db9bf6263fb6f9638d
Scala Steward <me@scala-steward.org>
2021-02-13 10:10:30 +0100

    Update awscala to 0.8.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val akka = "2.6.11"
     val akkaHttp = "10.1.11"
     val akkaHttpCirce = "1.35.3"
-    val awscala = "0.5.+"
+    val awscala = "0.8.5"
     val cats = "1.6.0"
     val catsEffect = "1.3.0"
     val monixCatnap = "3.0.0-RC2"
