97156872e89d145b61a40d4c18d643c5397d9c69
Scala Steward <me@scala-steward.org>
2021-03-25 12:12:10 +0100

    Update pprint to 0.6.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -33,7 +33,7 @@ object Dependencies {
     val scalameta = "4.3.22"
     val scalafmtCore = "2.0.0-RC5"
     val plantuml = "8059"
-    val pprint = "0.5.9"
+    val pprint = "0.6.3"
     val tapir = "0.14.4"
     val munit = "0.7.3"
   }
