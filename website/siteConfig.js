59e9552cef34e7afba124b616d35ae729e85282c
Gabriele Petronella <gabriele@buildo.io>
2021-05-06 23:20:54 +0200

    Fix header link

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -31,7 +31,7 @@ const siteConfig = {
     { doc: "sbt-buildo/introduction", label: "sbt-buildo" },
     { doc: "tapiro/introduction", label: "tapiro" },
     { doc: "mailo/introduction", label: "mailo" },
-    { doc: "mailo/introduction", label: "wiro" },
+    { doc: "wiro/installation", label: "wiro" },
     { search: true },
     {
       href: "https://github.com/buildo/retro",
